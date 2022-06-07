7426d5192c3c14388022f260cd2eb2cc6c11e2e7
Vinicio Leonardo Borja Tapia <vlborja@espe.edu.ec>
2022-06-07 13:33:16 -0500

    Agregar metodos burcar_dfs y obterner_ruta

--- lib/algoritmoBusqueda.py
+++ lib/algoritmoBusqueda.py
@@ -227,4 +227,21 @@ class Grafo:
                     cola.put(siguiente_nodo)
                     #marcarlo como visitado
                     visitado.add(siguiente_nodo)
+#Verificacion de script ejecutado como principal
+if __name__ == "__main__":
+    #Verices para crear arbol con su peso con su peso 
+    bordes_nodos = [(1,2,3.55),(1,3,3.64),(2,7,0.82),(2,8,4.62),
+            (3,6,2.39),(3,4,2.57),(3,5,2.63),(4,11,1.96),
+            (5,11,1.02),(5,12,1.65),(5,17,2.06),(5,6,3.20),(5,13,3.82),
+            (6,13,2.30),(6,7,2.36),(6,8,3.55),(6,16,6.81),
+            (8,9,0.5),(9,10,2.23),(11,12,1),(12,17,1.5),
+            (13,14,1),(13,18,2.77),(13,17,4.49),(14,15,1.5),
+            (14,18,2.08),(15,18,2.70),(15,16,2.97),(17,19,1.62),
+            (17,18,3.88),(18,20,2.45),(18,19,4.72),(19,21,2.90)]
+    # Crear un Ejemplo con una instancia de la clase `Grafo` con 5 nodos y no dirigido
+    g = Grafo(21, bordes_nodos,dirigido=False)
+
+
+    #print(g.bfs_transversal(5,4))
+    print(g.obtener_ruta(1,4))
 
